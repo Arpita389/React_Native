c11515bb120a2d6e7b03b32455aa431ac08be534
Arpita389 <138769882+Arpita389@users.noreply.github.com>
2024-07-05 08:33:12 +0530

    Add files via upload

---
+++ AuthContext.js
@@ -0,0 +1,34 @@
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const login = async (data) => {
+    setUser(data);
+    await AsyncStorage.setItem("user", JSON.stringify(data));
+  };
+
+  const logout = async () => {
+    setUser(null);
+    await AsyncStorage.removeItem("user");
+  };
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const storedUser = await AsyncStorage.getItem("user");
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    };
+    loadUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
